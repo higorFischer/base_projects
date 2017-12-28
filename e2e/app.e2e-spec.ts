4f1b954b5128ab6f8c40625339963e865b372886
Higor <higorflopes@gmail.com>
2017-12-28 20:14:00 -0200

    first commit

---
+++ e2e/app.e2e-spec.ts
@@ -0,0 +1,14 @@
+import { AppPage } from './app.po';
+
+describe('project-a4-b4-scss App', () => {
+  let page: AppPage;
+
+  beforeEach(() => {
+    page = new AppPage();
+  });
+
+  it('should display welcome message', () => {
+    page.navigateTo();
+    expect(page.getParagraphText()).toEqual('Welcome to app!');
+  });
+});
